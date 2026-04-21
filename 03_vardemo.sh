a84f735fdfcfc7dae666951c61218f55f6bc2b8a
gayatrishinde20 <shindegayatri895@gmail.com>
2026-04-21 22:44:10 +0530

    Add files via upload

---
+++ 03_vardemo.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+#defining variables
+
+readonly name="gayatri"
+age=20
+ 
+echo "my name is $name and age is $age
+"
+name="paul"
+echo "new name is ${name}"
+
+HOSTNAME=$(hostname)
+echo "Name of the server is $HOSTNAME"
+
+PWD=$(pwd)
+echo "ew are in path $PWD"
+
