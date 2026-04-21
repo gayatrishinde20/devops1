d462bb989b5d3e56a1b22e0e1a4022acc9f23b5f
gayatrishinde20 <shindegayatri895@gmail.com>
2026-04-21 22:50:51 +0530

    Add files via upload

---
+++ 15_forloop_cond.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+for num in 10 20 30 40 50 
+do
+	echo "number is $num"
+done
+
+for name in raju sham paul alex
+do
+	echo "name is $name"
+done
+
+for i in {1..10}
+do
+	echo "$i"
+done
